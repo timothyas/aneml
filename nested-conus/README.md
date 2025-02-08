575347a58098a9116126f33eb260e58dab82d88c
Timothy Andrew Smith <smith.timothy.andrew@gmail.com>
2025-02-07 16:12:08 -0800

    nested conus project hello world

---
+++ nested-conus/README.md
@@ -0,0 +1,7 @@
+# Nested CONUS
+
+Variable questions:
+- unsure if HRRR has total water, total water vapor .. has vertically integrated
+  liquied water, but unclear right now how that lines up with what is in the
+  ERA5 datasets
+  will skip for now
---
+++ nested-conus/era5-0.25/submit_dataset.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+#SBATCH -J era5-0.25
+#SBATCH -o slurm/dataset.%j.out
+#SBATCH -e slurm/dataset.%j.err
+#SBATCH --nodes=1
+#SBATCH --tasks-per-node=12
+#SBATCH --cpus-per-task=20
+#SBATCH --qos=regular
+#SBATCH --account=m4718
+#SBATCH --constraint=cpu
+#SBATCH -t 24:00:00
+
+log_dir=$COMMUNITY/aneml/logs
+output_path=$WORK/aneml/one-year.era5-0.25-degree.zarr
+
+mkdir -p $log_dir
+
+conda activate anemoi-datasets
+anemoi-datasets init recipe.dataset.yaml $output_path --overwrite > "$log_dir/init.log" 2>&1
+
+for i in $(seq 1 $SLURM_NTASKS); do
+  srun -n 1 anemoi-datasets load "$output_path" --part "$i/$SLURM_NTASKS" > "$log_dir/part_${i}.log" 2>&1 &
+done
+
+wait
