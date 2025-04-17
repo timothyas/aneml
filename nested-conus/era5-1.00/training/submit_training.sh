504440e9683a4b9dddc8b8b355447238ec1eb696
Timothy Andrew Smith <smith.timothy.andrew@gmail.com>
2025-04-17 08:14:01 -0700

    old updates

--- nested-conus/era5-1.00/training/submit_training.sh
+++ nested-conus/era5-1.00/training/submit_training.sh
@@ -3,14 +3,14 @@
 #SBATCH -J nested-conusc-era5-1.00-training
 #SBATCH -o slurm/training.%j.out
 #SBATCH -e slurm/training.%j.err
-#SBATCH --nodes=2
+#SBATCH --nodes=4
 #SBATCH --tasks-per-node=4
 #SBATCH --gpus-per-node=4
 #SBATCH --cpus-per-task=16
-#SBATCH --qos=debug
+#SBATCH --qos=regular
 #SBATCH --account=m4718
 #SBATCH --constraint=gpu&hbm80g
-#SBATCH -t 00:30:00
+#SBATCH -t 36:00:00
 
 conda activate anemoi-core
 
